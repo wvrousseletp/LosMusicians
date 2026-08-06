bb8e8428c0567c4ff3ff766264b314fa5d9cf079
Wéllerson Vicente Rousselet Porfírio <wellersonvicenterousseletporfirio@MacBook-Air-de-Wellerson.local>
2026-08-06 09:18:34 -0300

    ci: update bundle identifier to com.vicente.losmusicians in project settings, entitlements, and GoogleService-Info.plist

--- scripts/generate_entitlements.py
+++ scripts/generate_entitlements.py
@@ -5,11 +5,11 @@ runner_temp = os.environ.get("RUNNER_TEMP", "/tmp")
 output_path = os.path.join(runner_temp, "entitlements.plist")
 
 ents = {
-    "application-identifier": "PJ32UPL29J.com.vicente.losmooscles",
+    "application-identifier": "PJ32UPL29J.com.vicente.losmusicians",
     "com.apple.developer.team-identifier": "PJ32UPL29J",
     "get-task-allow": False,
     "beta-reports-active": True,
-    "keychain-access-groups": ["PJ32UPL29J.com.vicente.losmooscles"],
+    "keychain-access-groups": ["PJ32UPL29J.com.vicente.losmusicians"],
 }
 
 pathlib.Path(output_path).write_bytes(plistlib.dumps(ents))
